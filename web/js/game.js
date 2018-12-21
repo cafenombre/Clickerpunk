b11b91b704695c1620b3131fc080fa6ef91eb517
Joseph M <jmartell@etudiant.univ-lr.fr>
2018-12-21 15:05:00 +0100

    Restructuring the code, added comments

--- web/js/game.js
+++ web/js/game.js
@@ -1,3 +1,10 @@
+// IMPORTANT
+// rps: Robot Per Second
+// END IMPORTANT
+
+
+
+// Init the different game variables
 var Game = {};
 Game.robot_count = 0;
 Game.robot_factor_build = 1;
@@ -5,12 +12,20 @@ Game.build_sec_factories = 1;
 Game.slots = {"slot_1": 1};
 Game.number_factories = 100;
 Game.robot_level = 1;
-var robot_count_block = document.querySelector('.robot-count-content');
-var costs = {"factory_0": 10000, "factory_1": 100, "factory_2": 500, "factory_3": 5000, "upgrade_robot": 10};
 
+// Keep the block in a global var to save memory
+var robot_count_block = document.querySelector('.robot-count-content');
+
+// --- Static values @TODO: Should it stay as is or in a new json file ?
+// Costs using robots (first robot type)
+var costs = {"factory_0": 10000, "factory_1": 100, "factory_2": 500, "factory_3": 5000, "upgrade_robot": 10};
 // Number of robots per factory level
 var factor_factory_level = {"factory_-1": 0, "factory_0": 0, "factory_1": 1, "factory_2": 5, "factory_3": 10};
 
+/**
+ * Game updater
+ * Updates the robot built per second (e.g. used when a factory has been upgraded)
+ */
 function update_build_sec_factories() {
     var persec = 0;
     for (var slot in Game.slots) {
@@ -19,10 +34,19 @@ function update_build_sec_factories() {
     Game.build_sec_factories = persec;
 }
 
+/**
+ * Frontend updater
+ * Change the value of the counter on the page
+ */
 function update_clicker() {
     robot_count_block.innerHTML = String(Math.round(parseFloat(Game.robot_count)));
 }
 
+/**
+ * Frontend updater
+ * Fill cells which match the slot level (see Game.slots)
+ * Always update the rps
+ */
 function update_factories() {
     update_build_sec_factories()
 
@@ -35,50 +59,19 @@ function update_factories() {
     }
 }
 
+/**
+ * Frontend updater
+ * Changes the robot level
+ */
 function update_robot_details() {
     document.querySelector('.robot-level-content').innerHTML = Game.robot_level;
 }
 
-// type must be int (0 for factory_0, 1 for factory_1 ...)
-function price_factory(type) {
-    var count_f = 0;
-
-    for (s in Game.slots)
-        if (Game.slots[s] >= type)
-            count_f++;
-
-    count_f = (count_f === 0) ? 1:count_f;
-
-    return costs['factory_'+type] * count_f;
-}
-
-function can_afford_factory(request) {
-    return (Game.robot_count >= price_factory(request));
-}
-
-function can_afford_upgrade(level) {
-    return (Game.robot_count >= costs['upgrade_robot']*level*0.8)
-}
-
-function can_evolve_factory(slot, lvlRequest) {
-    var current_level = Game.slots[slot];
-    return (lvlRequest > current_level && lvlRequest - current_level === 1);
-}
-
-function pay_factory(request) {
-    if (price_factory(request) <= Game.robot_count)
-        Game.robot_count = Game.robot_count-price_factory(request);
-    update_clicker();
-    affordable();
-}
-
-function pay_robot(level) {
-    if (costs['upgrade_robot']*level*0.8 <= Game.robot_count)
-        Game.robot_count = Game.robot_count-costs['upgrade_robot']*level*0.8;
-    update_clicker();
-    affordable();
-}
-
+/**
+ * Frontend updater
+ * Highlight the affordable slots/factories/upgrades ...
+ *
+ */
 function affordable() {
     // Slots
     document.querySelectorAll('.town table').forEach(function (table) {
@@ -93,23 +86,108 @@ function affordable() {
     }
 }
 
-// AUTO BUILD
+/**
+ * Using the factory type (0 <= type <= 3), calculate the price of the next same type
+ * The second factory N will be more expensive than the first factory N
+ *
+ * @param   type      the level of the factory
+ * @returns {number}  the price
+ */
+function price_factory(type) {
+    var count_f = 0;
+
+    for (s in Game.slots)
+        if (Game.slots[s] >= type)
+            count_f++;
+
+    count_f = (count_f === 0) ? 1:count_f;
+
+    return costs['factory_'+type] * count_f;
+}
+
+/**
+ * Check if the user can buy a factory
+ *
+ * @param   request    the level of the the factory
+ * @returns {boolean}  the decision
+ */
+function can_afford_factory(request) {
+    return (Game.robot_count >= price_factory(request));
+}
+
+/**
+ * Check if the robot can be upgraded
+ *
+ * @param   level     the robot's level
+ * @returns {boolean} the decision
+ */
+function can_afford_upgrade(level) {
+    return (Game.robot_count >= costs['upgrade_robot']*level*0.8)
+}
+
+/**
+ * Check if the user can upgrade a factory
+ *
+ * @param   slot        the slot to check
+ * @param   lvlRequest  the level wanted (usually slot level + 1)
+ * @returns {boolean}   the decision
+ */
+function can_evolve_factory(slot, lvlRequest) {
+    var current_level = Game.slots[slot];
+    return (lvlRequest > current_level && lvlRequest - current_level === 1);
+}
+
+/**
+ * Pay for the factory
+ * Always recheck if it's affordable, update the clicker and recheck what the user can afford
+ *
+ * @param request  the factory level
+ */
+function pay_factory(request) {
+    if (price_factory(request) <= Game.robot_count)
+        Game.robot_count = Game.robot_count-price_factory(request);
+    update_clicker();
+    affordable();
+}
+
+/**
+ * Pay for a robot
+ * Always recheck if it's affordable, update the clicker and recheck what the user can afford
+ *
+ * @param level  the robot level
+ */
+function pay_robot(level) {
+    if (costs['upgrade_robot']*level*0.8 <= Game.robot_count)
+        Game.robot_count = Game.robot_count-costs['upgrade_robot']*level*0.8;
+    update_clicker();
+    affordable();
+}
+
+/**
+ * The automatic robot build function
+ * for the sake of display, this function is called every 100ms
+ * which means that the rps must be divided by 10
+ *
+ */
 function auto_build() {
     Game.robot_count += parseFloat(Game.build_sec_factories/10 * Game.robot_factor_build);
     update_clicker();
 }
 
-update_build_sec_factories();
-setInterval(auto_build, 100);
-setInterval(affordable, 2000);
+update_build_sec_factories(); // Init the rps
+setInterval(auto_build, 100); // auto build every 100 ms
+setInterval(affordable, 2000); // check affordable stuff but not every 100 ms ...
 
 
-// EVENTS
+// --- EVENTS
+
+// Clicker to build robots
 document.querySelector('.main-click').addEventListener('click', function () {
     Game.robot_count = Game.robot_count + Game.robot_level;
     update_clicker();
 });
 
+// Buy or upgrade a slot
 document.querySelectorAll('.town .slot').forEach( function (slot) {
    slot.addEventListener('click', function () {
         var number_slot = slot.getAttribute('data-slot');
@@ -129,6 +207,7 @@ document.querySelectorAll('.town .slot').forEach( function (slot) {
    })
 });
 
+// Upgrade robots
 document.querySelector('.upgrade-robot-level').addEventListener('click', function () {
     console.log(can_afford_upgrade(Game.robot_level+1));
     if (can_afford_upgrade(Game.robot_level+1)) {
