d4267b639ff386b35918293725d18ed113d3d070
Uderline <joseph.mss@hotmail.fr>
2019-01-02 18:00:53 +0100

    Added Lato font and minor other changes

--- views/common/head.php
+++ views/common/head.php
@@ -1,6 +1,7 @@
 <html>
 <head>
     <title>Clicker punk</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
     <link rel="stylesheet" href="/Clickerpunk/web/css/game.css">
 </head>
 <body>
